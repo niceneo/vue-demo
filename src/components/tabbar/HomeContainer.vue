6bda7fbaf31b11ddc4e7b19867ff133010e01d7c
Neo Jiang <niceneo666@gmail.com>
2019-08-07 11:01:17 +0800

    add tabbar

---
+++ src/components/tabbar/HomeContainer.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    <mt-swipe :auto="4000">
+      <mt-swipe-item v-for="item in lunbotuList" :key="item.url">
+        <img :src="item.img" alt="">
+      </mt-swipe-item>
+    </mt-swipe>
+
+    <ul class="mui-table-view mui-grid-view mui-grid-9">
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+              <span class="mui-icon mui-icon-home"></span>
+              <div class="mui-media-body">Home</div></a></li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+              <span class="mui-icon mui-icon-email"><span class="mui-badge">5</span></span>
+              <div class="mui-media-body">Email</div></a></li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+              <span class="mui-icon mui-icon-chatbubble"></span>
+              <div class="mui-media-body">Chat</div></a></li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+              <span class="mui-icon mui-icon-location"></span>
+              <div class="mui-media-body">location</div></a></li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+              <span class="mui-icon mui-icon-search"></span>
+              <div class="mui-media-body">Search</div></a></li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+              <span class="mui-icon mui-icon-phone"></span>
+              <div class="mui-media-body">Phone</div></a></li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+              <span class="mui-icon mui-icon-gear"></span>
+              <div class="mui-media-body">Setting</div></a></li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+              <span class="mui-icon mui-icon-info"></span>
+              <div class="mui-media-body">about</div></a></li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+              <span class="mui-icon mui-icon-more"></span>
+              <div class="mui-media-body">more</div></a></li>
+    </ul> 
+
+
+  </div>
+    
+</template>
+
+<script>
+import { Toast } from "mint-ui"
+
+export default {
+    data(){
+      return {
+        lunbotuList: []
+      }
+    },
+    created(){
+      this.getlunbotu();
+    },
+    methods: {
+      getlunbotu() {
+        this.$http.get("http://vue.studyit.io/api/getlunbo").then(result => {
+          if (result.body.status === 0 ) {
+            this.lunbotuList = result.body.message;
+          } else {
+            Toast("加载轮播图失败")
+          }
+        })
+      }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.mint-swipe {
+  height: 200px;
+
+  .mint-swipe-item {
+    &:nth-child(1) {
+      background-color: red;
+    }
+    &:nth-child(2) {
+      background-color: blue;
+    }
+    &:nth-child(3) {
+      background-color: cyan;
+    }
+  }
+}
+</style>
+
--- src/main.js
+++ src/main.js
@@ -1,2 +1,25 @@
-// 入口文件
-console.log('OK')
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+import VueResource from 'vue-resource'
+Vue.use(VueResource)
+
+
+import './lib/mui/css/mui.min.css'
+import './lib/mui/css/icons-extra.css'
+
+import { Header, Swipe, SwipeItem } from 'mint-ui'
+Vue.component(Header.name, Header)
+Vue.component(Swipe.name, Swipe)
+Vue.component(SwipeItem.name, SwipeItem)
+
+import router from './router.js'
+
+import app from './App.vue'
+
+var vm = new Vue({
+    el: '#app',
+    render: c => c(app),
+    router
+})
